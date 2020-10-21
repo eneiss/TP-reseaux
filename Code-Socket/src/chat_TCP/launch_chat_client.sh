ce893cc75d37dc07a166b68813e8cdd0df78e8f2
Neiss Emma <eneiss@if501-208-04.insa-lyon.fr>
2020-10-21 08:41:31 +0200

    Ajout de scripts pour chat TCP + serveur HTTP

---
+++ Code-Socket/src/chat_TCP/launch_chat_client.sh
@@ -0,0 +1,3 @@
+javac -d classes src/*.java
+java -classpath classes chat_TCP.src.ChatClient localhost 1234
+
---
+++ Code-Socket/src/chat_TCP/launch_chat_server.sh
@@ -0,0 +1,3 @@
+javac -d classes src/*.java
+java -classpath classes chat_TCP.src.ServerConnectionThread 1234
+
